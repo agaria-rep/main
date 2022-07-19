2f51752f7cdbc6cd3238b51c6700ac0ae61eeac0
piwer-m <96198356+agaria-rep@users.noreply.github.com>
2022-07-19 15:48:05 +0300

    index.js

--- decor/js/index.js
+++ decor/js/index.js
@@ -1,5 +1,10 @@
-
-
-if (document.documentElement.clientWidth < 556) {
-    document.querySelector('#zas h1').innerHTML = "НРА";
+function replays_zas(mode) {
+    if (mode == 1) {
+        document.querySelector('#zas h1').innerHTML = "НРА";
+    } else {
+        document.querySelector('#zas h1').innerHTML = "Народная Республика Агария";
+    }
 }
+
+if (document.documentElement.clientWidth < 556) {replays_zas(1);}
+window.onresize = function() {document.documentElement.clientWidth < 556 ? replays_zas(1) : replays_zas(0);}
