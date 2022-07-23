cce05a8aabe8cf2c82514346a72ec0a93768e160
piwer-m <96198356+agaria-rep@users.noreply.github.com>
2022-07-23 18:30:47 +0300

    css js html

--- decor/js/base.js
+++ decor/js/base.js
@@ -1,8 +1,8 @@
-let menu = "<header><a href=\"/\" class=\"logo\">НРА</a><nav><ul class=\"menu\"><li><a href=\"/\">Главная</a></li><li><a href=\"./news/\">Новости</a></li><li><a href=\"./info/\">Статьи</a></li></ul></nav></header>";
+let menu = "<header><a href=\"/\" class=\"logo\">НРА</a><nav><ul class=\"menu\"><li><a href=\"/\">Главная</a></li><li><a href=\"/news/\">Новости</a></li><li><a href=\"/info/\">Статьи</a></li></ul></nav></header>";
 
 document.querySelector('#menu').innerHTML = menu;
 
-let footer = "<footer><img src=\"media/imgs/logo.png\" alt=\"logo\" class=\"footer-img\"><div class=\"con\"><div class=\"nra\">НРА</div><a href=\"https://vk.com/agaria_official\" class=\"vk\">VK</a></div></footer>";
+let footer = "<footer><img src=\"./media/imgs/logo.png\" alt=\"logo\" class=\"footer-img\"><div class=\"con\"><div class=\"nra\">НРА</div><a href=\"https://vk.com/agaria_official\" class=\"vk\">VK</a></div></footer>";
 
 document.querySelector('#footer').innerHTML = footer;
 
