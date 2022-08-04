d43949a393c80786abeba28649d146a04762dc31
piwer-m <96198356+agaria-rep@users.noreply.github.com>
2022-08-04 22:04:20 +0300

    js css dark/light mode

--- decor/js/base.js
+++ decor/js/base.js
@@ -1,24 +1,36 @@
-if (false) {
+let mode = "normal";
+
+if (mode == "emergency") {
     window.location.replace("/errors/emergency-tech.html");
-} else if (window.location.toString().includes("errors") ) {
-    window.location.replace("/main");
-} else {
+} else if ((mode == "normal") || (mode == "tech")) {
 
     let addm = document.querySelector('#menu').innerHTML;
 
-    let menu = "<header><a href=\"/main\" class=\"logo\">НРА</a><nav><ul class=\"menu\"><li><a href=\"/main\">Главная</a></li><li><a href=\"/main/news/\">Новости</a></li><li><a href=\"/main/info/\">Статьи</a></li></ul></nav></header>";
+    let menu = `<header><a href="/main" class="logo">НРА</a><nav><a href="/main">Главная</a><a href="/main/news/">Новости</a><a href="/main/info/">Статьи</a></nav><button id="themeSwitch" onclick="toggleTheme()"><img src="${addm}media/imgs/theme/moon.png"></button></header>`;
 
     document.querySelector('#menu').innerHTML = menu;
 
     let addf = document.querySelector('#footer').innerHTML;
 
-    let footer = "<footer><img src=\""+addf +"media/imgs/logo.png\" alt=\"logo\" class=\"footer-img\"><div class=\"con\"><div class=\"nra\">НРА</div><a href=\"https://vk.com/agaria_official\" class=\"vk\">VK</a></div></footer>";
+    let footer = `<footer><img src="${addf}media/imgs/logo.png" alt="logo" class="footer-img"><div class="con"><div class="nra">НРА</div><a href="https://vk.com/agaria_official" class="vk">VK</a></div></footer>`;
 
     document.querySelector('#footer').innerHTML = footer;
+} else if (mode == "tech") {
+    let tech = `<span>Сайт на технических работах! </span><span>Не все может отображатся правильно!</span>`;
 
-    let tech = "<span>Сайт на технических работах! </span><span>Не все может отображатся правильно!</span>";
+    document.querySelector('#tech').innerHTML = tech;
+    document.querySelector('#tech').style.height = "75px"
+}
 
-    //document.querySelector('#tech').innerHTML = tech;
-    document.querySelector('#tech').style.height = "0px"
+if (mode == "normal") {
+    if (window.location.toString().includes("errors")) window.location = "/main";
+}
 
+function toggleTheme() {
+    document.querySelector(":root").classList.toggle("dark");
+    
+    if (document.querySelector(":root").classList.length > 0)
+        document.querySelector("#themeSwitch img").setAttribute("src", `media/imgs/theme/sun.png`);
+    else
+        document.querySelector("#themeSwitch img").setAttribute("src", `media/imgs/theme/moon.png`);
 }
